bb801beee9b71d7da930d9f9823f01dbe1f77e02
Rafaela <rafaela.o.barbosa@gmail.com>
2021-09-30 20:36:16 -0300

    [Feature Rafa - Task: Implemented ProdutoRepository]

---
+++ src/main/java/com/generation/MinhaLojadeGames/repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.generation.MinhaLojadeGames.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.MinhaLojadeGames.Model.ProdutoModel;
+
+public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long>{
+	public List<ProdutoModel> findAllByDescricaoContainingIgnoreCase(String nome);
+}
+
+
