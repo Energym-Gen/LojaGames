4e8b67eff1107acd536daf628b523e0345e8c207
Raniel Mendes <raniel.mendes@hotmail.com>
2021-10-13 09:44:42 -0300

    [Feature Rani - Task: Update UsuarioController]

--- src/main/java/com/generation/MinhaLojadeGames/controller/UsuarioController.java
+++ src/main/java/com/generation/MinhaLojadeGames/controller/UsuarioController.java
@@ -2,9 +2,6 @@ package com.generation.MinhaLojadeGames.controller;
 
 import java.util.Optional;
 
-import com.generation.MinhaLojadeGames.model.UserLogin;
-import com.generation.MinhaLojadeGames.model.Usuario;
-import com.generation.MinhaLojadeGames.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.generation.MinhaLojadeGames.Model.UserLogin;
+import com.generation.MinhaLojadeGames.Model.Usuario;
+import com.generation.MinhaLojadeGames.service.UsuarioService;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -24,14 +24,14 @@ public class UsuarioController {
 	
 	@PostMapping("/logar")
 	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user){
-		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
+		return usuarioService.logar(user).map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 	}
 	
 	@PostMapping("/cadastrar")
 	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario){
 		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(usuarioService.CadastrarUsuario(usuario));
+				.body(usuarioService.cadastrarUsuario(usuario));
 	}
 	
 
