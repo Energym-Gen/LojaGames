aef3aadcf71de7ff15ae49100e1e611474269dfe
Matheus <matheus.malmeida7@gmail.com>
2021-10-14 17:00:14 -0300

    [Feature Matheus - Task: Modify UsuarioController]

--- src/main/java/com/generation/MinhaLojadeGames/controller/UsuarioController.java
+++ src/main/java/com/generation/MinhaLojadeGames/controller/UsuarioController.java
@@ -1,38 +1,84 @@
 package com.generation.MinhaLojadeGames.controller;
 
+import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 import com.generation.MinhaLojadeGames.Model.UserLogin;
 import com.generation.MinhaLojadeGames.Model.Usuario;
+import com.generation.MinhaLojadeGames.repository.UsuarioRepository;
 import com.generation.MinhaLojadeGames.service.UsuarioService;
 
 @RestController
 @RequestMapping("/usuarios")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
-	
+
 	@Autowired
 	private UsuarioService usuarioService;
-	
+
+	@Autowired
+	private UsuarioRepository repository;
+
 	@PostMapping("/logar")
-	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user){
+	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user) {
 		return usuarioService.logar(user).map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 	}
-	
+
 	@PostMapping("/cadastrar")
-	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario){
-		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(usuarioService.cadastrarUsuario(usuario));
+	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.cadastrarUsuario(usuario));
 	}
-	
+
+	@GetMapping
+	public ResponseEntity<List<Usuario>> buscarTodosUsuarios() {
+		List<Usuario> objetoLista = repository.findAll();
+
+		if (objetoLista.isEmpty()) {
+			return ResponseEntity.status(204).build();
+		} else {
+			return ResponseEntity.status(200).body(objetoLista);
+		}
+	}
+
+	@GetMapping("/{id_usuario}")
+	public ResponseEntity<Usuario> buscarPorId(@PathVariable(value = "id_usuario") Long idUsuario) {
+		return repository.findById(idUsuario).map(idExistente -> ResponseEntity.status(200).body(idExistente))
+				.orElseThrow(() -> {
+					throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+							"ID inexistente, passe um ID valido para pesquisa!.");
+				});
+	}
+
+	@PutMapping("/atualizar")
+	public ResponseEntity<Usuario> atualizar(@Valid @RequestBody Usuario novoUsuario) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.cadastrarUsuario(novoUsuario));
+	}
+
+	@DeleteMapping("/deletar/{id_usuario}")
+	public ResponseEntity<Object> deletar(@PathVariable(value = "id_usuario") Long idUsuario) {
+		return repository.findById(idUsuario).map(idExistente -> {
+			repository.deleteById(idUsuario);
+			return ResponseEntity.status(200).build();
+		}).orElseThrow(() -> {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "ID inexistente, passe um ID valido para deletar!");
+		});
+	}
 
 }
